16b06b614cc9b79981b110bb6810f1c4bb5e02bb
ReshmaKuzhandaivel <reshma20020910@gmail.com>
2023-09-25 18:33:31 +0530

    first commit

---
+++ src/test/java/Attributes/Harddependsonmethod.java
@@ -0,0 +1,44 @@
+package Attributes;
+
+import org.testng.annotations.Test;
+
+public class Softdependsonmethod {
+
+    @Test(dependsOnMethods = {"loginTest"})
+    public void sendMessageTest() {
+        System.out.println("Test is executed!!!");
+        try {
+            Thread.sleep(5000);
+        } catch (Exception e) {}
+    }
+
+    @Test(dependsOnMethods = {"loginTest"})
+    public void postPictureTest() {
+        System.out.println("Test");
+        try {
+            Thread.sleep(5000);
+        } catch (Exception e) {}
+    }
+
+    @Test
+    public void registerAccountTest() {
+        System.out.println("Test3");
+        try {
+            Thread.sleep(5000);
+        } catch (Exception e) {}
+    }
+
+    @Test(dependsOnMethods = {"registerAccountTest"})
+    public void loginTest() {
+        System.out.println("Test4");
+        try {
+            Thread.sleep(5000);
+        } catch (Exception e) {}
+    }
+
+    @Test
+    public void test() {
+        System.out.println("Test without dependency");
+    }
+}
+
---
+++ src/test/java/Test/IncludeExclude.java
@@ -0,0 +1,31 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class IncludeExclude {
+
+    @Test
+    public void Me() {
+
+        System.out.println("resh");
+    }
+
+    @Test
+    public void Pisasu() {
+        System.out.println("sham");
+
+    }
+
+    @Test
+    public void Egoistic() {
+        System.out.println("sago");
+
+    }
+
+    @Test
+    public void Care() {
+        System.out.println("Bro");
+
+    }
+}
+
